e9a27563faca441d6f87b8bd83cde0cbab97596b
Denis Shikunets <denis@origins-digital.com>
2023-01-17 17:22:46 +0300

    fix: change some modifiers to public

--- Demo/SampleShimmerController1.swift
+++ Demo/SampleShimmerController1.swift
@@ -8,6 +8,8 @@
 import UIKit
 import Alidade
 import RxSwift
+import Moondial
+import PinLayout
 
 class SampleShimmerController1: UIViewController, UIGestureRecognizerDelegate {
     
@@ -19,4 +21,34 @@ class SampleShimmerController1: UIViewController, UIGestureRecognizerDelegate {
     func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
         return true
     }
+}
+
+class ShimmerDemoView: UIView, ShimmersContainer {
+    
+    private let logo = UIImageView()
+    private let text = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addSubviews(logo, text)
+        text.text = "Demo Text!!!"
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        logo.pin.vCenter().start(20.ui)
+        text.pin.after(of: logo).vCenter().marginStart(10.ui).sizeToFit(.width)
+    }
+    
+    var shimmers: [ShimmerSettings] {
+       return [
+        logo.shimmer(style: .transparent).width(40.ui).height(40.ui),
+        text.shimmer(style: .transparent)
+       ]
+    }
+    
 }
--- ShimmerSettings.swift
+++ ShimmerSettings.swift
@@ -9,42 +9,42 @@
 import UIKit
 
 public enum ShimmerStyle {
-  case opaque
-  case transparent
-  case hidden // hide view when shimmering
+    case opaque
+    case transparent
+    case hidden // hide view when shimmering
 }
 
-enum ShimmerAlignment {
-  case start
-  case end
-  case center
+public enum ShimmerAlignment {
+    case start
+    case end
+    case center
 }
 
 protocol Shimmerable {
-
-  var views: [UIView] { get }
-  var style: ShimmerStyle { get }
-
-  var cornerRadius: CGFloat? { get }
-  var height: CGFloat? { get }
-  var width: CGFloat? { get }
-  var hAlignment: ShimmerAlignment? { get }
-  var vAlignment: ShimmerAlignment? { get }
-
-  var customShape: ((CGRect) -> (UIBezierPath))? { get }
-
-  var subshimmers: [Shimmerable] { get }
+    
+    var views: [UIView] { get }
+    var style: ShimmerStyle { get }
+    
+    var cornerRadius: CGFloat? { get }
+    var height: CGFloat? { get }
+    var width: CGFloat? { get }
+    var hAlignment: ShimmerAlignment? { get }
+    var vAlignment: ShimmerAlignment? { get }
+    
+    var customShape: ((CGRect) -> (UIBezierPath))? { get }
+    
+    var subshimmers: [Shimmerable] { get }
 }
 
 protocol ShimmerConvertible {
-  func asShimmers() -> [ShimmerSettings]
+    func asShimmers() -> [ShimmerSettings]
 }
 
 extension ShimmerSettings: ShimmerConvertible {
-
-  func asShimmers() -> [ShimmerSettings] {
-    [self]
-  }
+    
+    func asShimmers() -> [ShimmerSettings] {
+        [self]
+    }
 }
 
 extension Array: ShimmerConvertible where Element == ShimmerSettings {
@@ -52,81 +52,81 @@ extension Array: ShimmerConvertible where Element == ShimmerSettings {
 }
 
 @resultBuilder struct ShimmerBuilder {
-
-  typealias Component = ShimmerConvertible
-
-  static func buildBlock(_ components: ShimmerConvertible...) -> [ShimmerConvertible] {
-    components
-  }
+    
+    typealias Component = ShimmerConvertible
+    
+    static func buildBlock(_ components: ShimmerConvertible...) -> [ShimmerConvertible] {
+        components
+    }
 }
 
-protocol ShimmersContainer {
-  var shimmers: [ShimmerSettings] { get }
+public protocol ShimmersContainer {
+    var shimmers: [ShimmerSettings] { get }
 }
 
-class ShimmerSettings: Shimmerable {
-
-  let views: [UIView]
-  let style: ShimmerStyle
-
-  init(views: [UIView], style: ShimmerStyle) {
-    self.views = views
-    self.style = style
-  }
-
-  func width(_ value: CGFloat) -> ShimmerSettings {
-    self.width = value
-    return self
-  }
-
-  func height(_ value: CGFloat) -> ShimmerSettings {
-    self.height = value
-    return self
-  }
-
-  func cornerRadius(_ value: CGFloat) -> ShimmerSettings {
-    self.cornerRadius = value
-    return self
-  }
-
-  func customShape(_ value: @escaping (CGRect) -> (UIBezierPath)) -> ShimmerSettings {
-    self.customShape = value
-    return self
-  }
-
-  func vAlignment(_ value: ShimmerAlignment) -> ShimmerSettings {
-    self.vAlignment = value
-    return self
-  }
-
-  func hAlignment(_ value: ShimmerAlignment) -> ShimmerSettings {
-    self.hAlignment = value
-    return self
-  }
-
-  func subshimmers( _ value: [Shimmerable]) -> ShimmerSettings {
-    self.subshimmers = value
-    return self
-  }
-
-  var height: CGFloat?
-  var width: CGFloat?
-  var cornerRadius: CGFloat?
-  var hAlignment: ShimmerAlignment?
-  var vAlignment: ShimmerAlignment?
-  var customShape: ((CGRect) -> (UIBezierPath))?
-  var subshimmers: [Shimmerable] = []
+public class ShimmerSettings: Shimmerable {
+    
+    let views: [UIView]
+    let style: ShimmerStyle
+    
+    init(views: [UIView], style: ShimmerStyle) {
+        self.views = views
+        self.style = style
+    }
+    
+    public func width(_ value: CGFloat) -> ShimmerSettings {
+        self.width = value
+        return self
+    }
+    
+    public func height(_ value: CGFloat) -> ShimmerSettings {
+        self.height = value
+        return self
+    }
+    
+    public func cornerRadius(_ value: CGFloat) -> ShimmerSettings {
+        self.cornerRadius = value
+        return self
+    }
+    
+    public func customShape(_ value: @escaping (CGRect) -> (UIBezierPath)) -> ShimmerSettings {
+        self.customShape = value
+        return self
+    }
+    
+    public func vAlignment(_ value: ShimmerAlignment) -> ShimmerSettings {
+        self.vAlignment = value
+        return self
+    }
+    
+    public func hAlignment(_ value: ShimmerAlignment) -> ShimmerSettings {
+        self.hAlignment = value
+        return self
+    }
+    
+    func subshimmers( _ value: [Shimmerable]) -> ShimmerSettings {
+        self.subshimmers = value
+        return self
+    }
+    
+    var height: CGFloat?
+    var width: CGFloat?
+    var cornerRadius: CGFloat?
+    var hAlignment: ShimmerAlignment?
+    var vAlignment: ShimmerAlignment?
+    var customShape: ((CGRect) -> (UIBezierPath))?
+    var subshimmers: [Shimmerable] = []
 }
 
-extension UIView {
-  func shimmer(style: ShimmerStyle) -> ShimmerSettings {
-    return ShimmerSettings(views: [self], style: style)
-  }
+public extension UIView {
+    func shimmer(style: ShimmerStyle) -> ShimmerSettings {
+        return ShimmerSettings(views: [self], style: style)
+    }
 }
 
-extension Array where Element: UIView {
-  func shimmer(style: ShimmerStyle) -> ShimmerSettings {
-    return ShimmerSettings(views: self, style: style)
-  }
+public extension Array where Element: UIView {
+    func shimmer(style: ShimmerStyle) -> ShimmerSettings {
+        return ShimmerSettings(views: self, style: style)
+    }
 }
 
